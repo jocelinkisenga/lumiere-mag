9799e39b185394dc2952baedf0edec199f34c787
kisenga shuka jocelin <72705409+jocelinkisenga@users.noreply.github.com>
2026-06-22 05:30:20 +0200

    Update listArticles.blade.php

--- resources/views/pages/admin/listArticles.blade.php
+++ resources/views/pages/admin/listArticles.blade.php
@@ -52,12 +52,12 @@
                                     </td>
                                     <td class="text-end">
                                         <div class="btn-group shadow-sm">
-                                            <a href="{{ route('posts.edit', ['id' => $item->id]) }}" 
+                                            <a href="{{ route('posts.edit', $item->id) }}" 
                                                class="btn btn-success btn-sm" 
                                                onclick="return confirm('Supprimer cet article ?')">
                                                 <i class="fas fa-edit"></i>
                                             </a>
-                                            <a href="{{ route('post.delete', ['id' => $item->id]) }}" 
+                                            <a href="{{ route('post.delete', ['id' => $item->id])}}" 
                                                class="btn btn-danger btn-sm" 
                                                onclick="return confirm('Supprimer cet article ?')">
                                                 <i class="fas fa-trash"></i>
